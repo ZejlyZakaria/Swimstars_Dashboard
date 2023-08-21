6c7a332d68f9b029c2e711ed2340412e9e3fb04f
Zakaria <zejly12@gmail.com>
2023-08-21 21:54:55 +0100

    add vuejs

---
+++ swimstarsapp/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Je suis un composant Vue</h2>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Bonjour depuis le composant Vue !",
+        };
+    },
+};
+</script>
